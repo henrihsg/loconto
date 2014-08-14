1ce5098635b2fe8f8efa22d766d37bfc552b52f4
henrihsg <henrihs.g@gmail.com>
2014-08-14 16:51:16 +0300

    testing

--- LocOnto/src/lv/loconto/Main.java
+++ LocOnto/src/lv/loconto/Main.java
@@ -1,9 +1,31 @@
 package lv.loconto;
 
+import com.hp.hpl.jena.query.Dataset;
+import com.hp.hpl.jena.query.ReadWrite;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.tdb.TDBFactory;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//Make a TDB-backed dataset
+		String directory = "BD/Dataset1" ;
+		Dataset dataset = TDBFactory.createDataset(directory) ;
+		
+		Model model = ModelFactory.createDefaultModel();
+		
+		dataset.begin(ReadWrite.WRITE);
+		dataset.addNamedModel("Test", model);
+		
+		/*dataset.begin(ReadWrite.READ) ;
+		// Get model inside the transaction
+		Model model = dataset.getDefaultModel() ;
+		dataset.end() ;
+		dataset.begin(ReadWrite.WRITE) ;
+		model = dataset.getDefaultModel() ;*/
+		dataset.end() ;
 
 	}
 
